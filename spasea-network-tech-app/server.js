b5f3b515778785f54da48ba2df685e8692bc791e
epson agunda <epsonagunda182@gmail.com>
2024-08-08 08:10:51 -0700

    spasea

--- spasea-network-tech-app/server.js
+++ spasea-network-tech-app/server.js
@@ -1,9 +1,9 @@
 // server.js
 
-const express = require('express'):
-const path = require('path'):
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 
 
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'ping.html'));
 });
 
-app.listen(port, () +> {
+app.listen(port, () => {
 	console.log('server running at http://localhost:${port}');
 });
 
@@ -27,10 +27,11 @@ app.get('/ping', async (req, res) => {
 	const hostname = req.query.hostname;
 
 	if (!hostname) {
-		return res.status(400).send('Hostname is required.');         }
+		return res.status(400).send('Hostname is required.');         
+	}
 	try { const result = await ping.promise.probe(hostname);
-		res.send('Host ${hostname} is ${result.alive ? 'alive' : 'dead'}\nRound-trip t
-} catch (error) {
+		res.send(`Host ${hostname} is ${result.alive ? 'alive' : 'dead'}\nRound-trip time: ${result.roundTripTime} ms`);
+	} catch (error) {
 	res.status(500).send('ping failed: ' + error.message);
-}
-);
+	}
+});
